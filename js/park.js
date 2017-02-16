237dff49f3232aedc4f71c6c8bfc92681c533f54
cameron jensen and jahan walsh <jahan@>
2017-02-16 11:56:37 -0800

    add class in css to highlight in jquery.

--- js/park.js
+++ js/park.js
@@ -1,11 +1,14 @@
 $(function(){
   var height = parseInt(prompt("Please put height in inches to see what rides are available."));
-
+debugger;
   if ( height < 48) {
     $('#simple').show();
+    $('#simple1').addClass("highlighted");
   } else if (height >= 48 && height < 55) {
     $('#medium').show();
+    $('#medium1').addClass("highlighted");
   } else {
     $('#crazy').show();
+    $('#crazy1').addClass("highlighted");
   }
 });
