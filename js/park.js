c9560966dda6a3b5d959e151b0954c31ec930568
cameron jensen and jahan walsh <jahan@>
2017-02-16 10:54:01 -0800

    add display none style to css. Add backend comments and adjust stylesheet link. Add conditions to if staments.

--- js/park.js
+++ js/park.js
@@ -1,3 +1,11 @@
 $(function(){
   var height = parseInt(prompt("Please put height in inches to see what rides are available."));
+
+  if ( height < 48) {
+    $('#simple').show();
+  } else if (height >= 48 && height < 55) {
+    $('#medium').show();
+  } else {
+    $('#crazy').show();
+  }
 });
